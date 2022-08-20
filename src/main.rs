86912f6f1930d37fc2f6bd632a31032b129788b2
MartMihkelAun <Mart.Mihkel.Aun@gmail.com>
2022-08-20 15:01:16 +0300

    minor refactor

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,21 @@ use std::f32::consts::TAU;
 use sfml::graphics::{RenderWindow, Color, RenderTarget, PrimitiveType, Vertex, RenderStates};
 use sfml::window::{Style, Event, Key};
 
-const SCREEN_WIDTH: u32 = 800;
-const SCREEN_HEIGHT: u32 = 400;
+const WIDTH: u32 = 800;
+const HEIGHT: u32 = 400;
 
-const X_ORIGIN: f32 = SCREEN_WIDTH as f32 / 2.;
-const Y_ORIGIN: f32 = SCREEN_HEIGHT as f32 / 2.;
+const X_ORIGIN: f32 = WIDTH as f32 / 2.;
+const Y_ORIGIN: f32 = HEIGHT as f32 / 2.;
 
 const CROSS_SECTION_RADIUS: f32 = 10.;
 const REVOLUTION_RADIUS: f32 = 15.;
 
 const OBJECT_DISTANCE: f32 = CROSS_SECTION_RADIUS + REVOLUTION_RADIUS + 5.;
-const PROJECTION_DISTANCE: f32 = SCREEN_WIDTH as f32 * OBJECT_DISTANCE * 3. / (8. * (CROSS_SECTION_RADIUS * REVOLUTION_RADIUS));
+const PROJECTION_DISTANCE: f32 = WIDTH as f32 * OBJECT_DISTANCE * 3. / (8. * (CROSS_SECTION_RADIUS * REVOLUTION_RADIUS));
 
 fn main() {
     let mut window = RenderWindow::new(
-        (SCREEN_WIDTH, SCREEN_HEIGHT),
+        (WIDTH, HEIGHT),
         "Rotating solid of revolution",
         Style::DEFAULT,
         &Default::default(),
@@ -27,7 +27,7 @@ fn main() {
 
     let mut vertices = [Vertex::default(); 2_usize.pow(12)];
 
-    let (mut y_rotation_angle, mut z_rotation_angle): (f32, f32) = (0., 0.);
+    let (mut yaw_angle, mut roll_angle): (f32, f32) = (0., 0.);
     while window.is_open() {
         // event loop
         while let Some(event) = window.poll_event() {
@@ -47,8 +47,8 @@ fn main() {
         // rendering
         window.clear(Color::BLACK);
 
-        let (y_rotation_sin, y_rotation_cos) = y_rotation_angle.sin_cos();
-        let (z_rotation_sin, z_rotation_cos) = z_rotation_angle.sin_cos();
+        let (yaw_angle_sin, yaw_angle_cos) = yaw_angle.sin_cos();
+        let (roll_angle_sin, roll_angle_cos) = roll_angle.sin_cos();
 
         // solid loop
         let mut vertex_i = 0;
@@ -57,16 +57,16 @@ fn main() {
             let (solid_sin, solid_cos) = solid_angle.sin_cos();
 
             // revolution loop
-            let mut revolution_angle = 0.;
-            while revolution_angle < TAU {
-                let (revolution_sin, revolution_cos) = revolution_angle.sin_cos();
+            let mut pitch_angle = 0.;
+            while pitch_angle < TAU {
+                let (pitch_sin, pitch_cos) = pitch_angle.sin_cos();
 
                 let v0 = REVOLUTION_RADIUS + CROSS_SECTION_RADIUS * solid_cos;
-                let v1 = CROSS_SECTION_RADIUS * y_rotation_cos * solid_sin;
+                let v1 = CROSS_SECTION_RADIUS * yaw_angle_cos * solid_sin;
                 let (x, y, z) = (
-                    v0 * (z_rotation_cos * revolution_cos + y_rotation_sin * z_rotation_sin * revolution_sin) - v1 * z_rotation_sin,
-                    v0 * (revolution_cos * y_rotation_sin - z_rotation_cos * y_rotation_sin * revolution_sin) + v1 * z_rotation_cos,
-                    v0 * y_rotation_cos * revolution_sin + CROSS_SECTION_RADIUS * y_rotation_sin * solid_sin,
+                    v0 * (roll_angle_cos * pitch_cos + yaw_angle_sin * roll_angle_sin * pitch_sin) - v1 * roll_angle_sin,
+                    v0 * (pitch_cos * yaw_angle_sin - roll_angle_cos * yaw_angle_sin * pitch_sin) + v1 * roll_angle_cos,
+                    v0 * yaw_angle_cos * pitch_sin + CROSS_SECTION_RADIUS * yaw_angle_sin * solid_sin,
                 );
 
                 let projection_multiplier = PROJECTION_DISTANCE / (z + OBJECT_DISTANCE);
@@ -74,12 +74,12 @@ fn main() {
                 vertices[vertex_i].position.x = X_ORIGIN + x * projection_multiplier;
                 vertices[vertex_i].position.y = Y_ORIGIN - y * projection_multiplier;
 
-                let luminance = revolution_cos * solid_cos * z_rotation_sin - y_rotation_cos * solid_cos * revolution_sin - y_rotation_sin * solid_sin + z_rotation_cos * (y_rotation_cos * solid_sin - solid_cos * y_rotation_sin * revolution_sin);
+                let luminance = pitch_cos * solid_cos * roll_angle_sin - yaw_angle_cos * solid_cos * pitch_sin - yaw_angle_sin * solid_sin + roll_angle_cos * (yaw_angle_cos * solid_sin - solid_cos * yaw_angle_sin * pitch_sin);
 
                 vertices[vertex_i].color = if luminance > 0. { Color::WHITE } else { Color::BLACK };
 
                 vertex_i += 1;
-                revolution_angle += 0.1;
+                pitch_angle += 0.1;
             }
 
             solid_angle += 0.1;
@@ -92,7 +92,7 @@ fn main() {
         );
         window.display();
 
-        y_rotation_angle += 0.03;
-        z_rotation_angle += 0.03;
+        yaw_angle += 0.03;
+        roll_angle += 0.03;
     }
 }
