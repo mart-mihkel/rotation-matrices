c70319b70d0f45d161db1bfb6eee6a37750e863c
MartMihkelAun <mart.mihkel.aun@gmail.com>
2023-07-02 21:24:01 +0300

    explanation

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Spinning Solid of Revolution
-
-Made using [Rust SFML](https://github.com/jeremyletang/rust-sfml)
-
-[Guide](https://www.a1k0n.net/2011/07/20/donut-math.html)
-
-![donut](assets/donut.gif)
+# Spinning thing
--- src/main.rs
+++ src/main.rs
@@ -1,98 +1,83 @@
 use std::f32::consts::TAU;
-
 use sfml::graphics::{RenderWindow, Color, RenderTarget, PrimitiveType, Vertex, RenderStates};
-use sfml::window::{Style, Event, Key};
-
-const WIDTH: u32 = 800;
-const HEIGHT: u32 = 400;
-
-const X_ORIGIN: f32 = WIDTH as f32 / 2.;
-const Y_ORIGIN: f32 = HEIGHT as f32 / 2.;
-
-const CROSS_SECTION_RADIUS: f32 = 10.;
-const REVOLUTION_RADIUS: f32 = 15.;
-
-const OBJECT_DISTANCE: f32 = CROSS_SECTION_RADIUS + REVOLUTION_RADIUS + 5.;
-const PROJECTION_DISTANCE: f32 = WIDTH as f32 * OBJECT_DISTANCE * 3. / (8. * (CROSS_SECTION_RADIUS * REVOLUTION_RADIUS));
+use sfml::system::{Vector2f, Vector2u};
+use sfml::window::{Style, Event, Key, VideoMode};
 
 fn main() {
+    let window_size = Vector2u::new(1920, 1080);
+    let window_size_f: Vector2f = window_size.as_other();
     let mut window = RenderWindow::new(
-        (WIDTH, HEIGHT),
-        "Rotating solid of revolution",
-        Style::DEFAULT,
+        VideoMode::new(window_size.x, window_size.y, 24),
+        "Spinning thing",
+        Style::FULLSCREEN,
         &Default::default(),
     );
 
     window.set_framerate_limit(60);
+    window.set_mouse_cursor_visible(false);
 
-    let mut vertices = [Vertex::default(); 2_usize.pow(12)];
+    let mut vertices = vec![Vertex::with_pos_color(Vector2f::default(), Color::WHITE); 2_usize.pow(12)];
+    let mut yaw: f32 = 0.0;
+    let mut roll: f32 = 0.0;
+    let mut pitch: f32 = 0.0;
 
-    let (mut yaw_angle, mut roll_angle): (f32, f32) = (0., 0.);
     while window.is_open() {
         // event loop
         while let Some(event) = window.poll_event() {
             match event {
                 Event::Closed | Event::KeyPressed { code: Key::Escape, .. } => window.close(),
-                Event::KeyPressed { code: Key::P, .. } => {
-                    while let Some(event) = window.wait_event() {
-                        if let Event::KeyReleased { code: Key::P, .. } = event {
-                            break;
-                        }
-                    }
-                },
                 _ => {}
             }
         }
 
-        // rendering
-        window.clear(Color::BLACK);
+        // iterate over all knots
+        let mut i = 0;
+        let mut knot: f32 = 0.0;
+        while knot <= 1.0 {
 
-        let (yaw_angle_sin, yaw_angle_cos) = yaw_angle.sin_cos();
-        let (roll_angle_sin, roll_angle_cos) = roll_angle.sin_cos();
+            // roll around the x-axis and apply the current pitch and yaw
+            while roll < TAU {
+                let (yaw_s, yaw_c) = yaw.sin_cos();
+                let (roll_s, roll_c) = roll.sin_cos();
+                let (pitch_s, pitch_c) = pitch.sin_cos();
 
-        // solid loop
-        let mut vertex_i = 0;
-        let mut solid_angle = 0.;
-        while solid_angle < TAU {
-            let (solid_sin, solid_cos) = solid_angle.sin_cos();
+                let f_knot = curve_f(knot);
 
-            // revolution loop
-            let mut pitch_angle = 0.;
-            while pitch_angle < TAU {
-                let (pitch_sin, pitch_cos) = pitch_angle.sin_cos();
+                // multiply all the rotation matrices and the coordinate column of the current knot
+                let x = knot * pitch_c * yaw_c + f_knot * (roll_s * pitch_s * yaw_c - roll_c * yaw_s);
+                let y = knot * pitch_c * yaw_s + f_knot * (roll_s * pitch_s * yaw_s + roll_c * yaw_c);
+                let z = knot * -pitch_s + f_knot * roll_s * pitch_c;
 
-                let v0 = REVOLUTION_RADIUS + CROSS_SECTION_RADIUS * solid_cos;
-                let v1 = CROSS_SECTION_RADIUS * yaw_angle_cos * solid_sin;
-                let (x, y, z) = (
-                    v0 * (roll_angle_cos * pitch_cos + yaw_angle_sin * roll_angle_sin * pitch_sin) - v1 * roll_angle_sin,
-                    v0 * (pitch_cos * yaw_angle_sin - roll_angle_cos * yaw_angle_sin * pitch_sin) + v1 * roll_angle_cos,
-                    v0 * yaw_angle_cos * pitch_sin + CROSS_SECTION_RADIUS * yaw_angle_sin * solid_sin,
-                );
+                // project points onto a screen on the z-axis
+                let object_distance = 2.5;
+                let screen_distance = window_size_f.x * object_distance * 0.04;
 
-                let projection_multiplier = PROJECTION_DISTANCE / (z + OBJECT_DISTANCE);
+                vertices[i].position.x = window_size_f.x * 0.5 + x * screen_distance / (z + object_distance);
+                vertices[i].position.y = window_size_f.y * 0.5 - y * screen_distance / (z + object_distance);
 
-                vertices[vertex_i].position.x = X_ORIGIN + x * projection_multiplier;
-                vertices[vertex_i].position.y = Y_ORIGIN - y * projection_multiplier;
+                i += 1;
 
-                let luminance = pitch_cos * solid_cos * roll_angle_sin - yaw_angle_cos * solid_cos * pitch_sin - yaw_angle_sin * solid_sin + roll_angle_cos * (yaw_angle_cos * solid_sin - solid_cos * yaw_angle_sin * pitch_sin);
-
-                vertices[vertex_i].color = if luminance > 0. { Color::WHITE } else { Color::BLACK };
-
-                vertex_i += 1;
-                pitch_angle += 0.1;
+                roll += 0.05;
             }
+            roll = 0.0;
 
-            solid_angle += 0.1;
+            knot += 0.05;
         }
 
+         yaw += 0.02;
+         pitch += 0.03;
+
+        // rendering
+        window.clear(Color::BLACK);
         window.draw_primitives(
-            &vertices[..vertex_i],
-            PrimitiveType::LINES,
+            &vertices[..i],
+            PrimitiveType::POINTS,
             &RenderStates::DEFAULT,
         );
         window.display();
-
-        yaw_angle += 0.03;
-        roll_angle += 0.03;
     }
+}
+
+fn curve_f(knot: f32) -> f32 {
+    -6.66667 * knot.powi(4) + 5.66667 * knot.powi(2) + 1.0
 }
